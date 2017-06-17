b58166fae3b28b9b5867683afd59332b996a57cb
allen9408 <724330304@qq.com>
2017-06-17 17:24:36 -0400

    Bug fix
    Signed-off-by: allen9408 <724330304@qq.com>

--- rollout.py
+++ rollout.py
@@ -77,7 +77,8 @@ class ROLLOUT(object):
     def get_reward(self, sess, input_x, rollout_num, discriminator):
         rewards = []
         for i in range(rollout_num):
-            for given_num in range(1, 20):
+            # for given_num in range(1, 20):
+            for given_num in range(1, 253):
                 feed = {self.x: input_x, self.given_num: given_num}
                 samples = sess.run(self.gen_x, feed)
                 feed = {discriminator.input_x: samples, discriminator.dropout_keep_prob: 1.0}
@@ -95,7 +96,8 @@ class ROLLOUT(object):
             if i == 0:
                 rewards.append(ypred)
             else:
-                rewards[19] += ypred
+                rewards[252] += ypred
+                # rewards[19] += ypred
 
         rewards = np.transpose(np.array(rewards)) / (1.0 * rollout_num)  # batch_size x seq_length
         return rewards
